844a20ab41a5047ad063c5acb73283b2f98f3350
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-09-09 16:26:41 -0700

    WIP round numbers

--- src/index.js
+++ src/index.js
@@ -15,14 +15,14 @@ function getExchange() {
   // ui
   function printElements(data) {
     const currencySelection = document.getElementById("currency-selection").value;
-    const AED = document.getElementById("dollar").value * `${[data[0].conversion_rates.AED]}`
-    const AFN = document.getElementById("dollar").value * `${[data[0].conversion_rates.AFN]}`
-    const ALL = document.getElementById("dollar").value * `${[data[0].conversion_rates.ALL]}`
-    const AMD = document.getElementById("dollar").value * `${[data[0].conversion_rates.AMD]}`
-    const ANG = document.getElementById("dollar").value * `${[data[0].conversion_rates.ANG]}`
+    const AED = document.getElementById("dollar").value * `${[data[0].conversion_rates.AED]}`;
+    const AFN = document.getElementById("dollar").value * `${[data[0].conversion_rates.AFN]}`;
+    const ALL = document.getElementById("dollar").value * `${[data[0].conversion_rates.ALL]}`;
+    const AMD = document.getElementById("dollar").value * `${[data[0].conversion_rates.AMD]}`;
+    const ANG = document.getElementById("dollar").value * `${[data[0].conversion_rates.ANG]}`;
 
     if (currencySelection === "AED") {
-      document.getElementById('exchange').innerText = AED;
+     document.getElementById('exchange').innerText = AED;
     } else if (currencySelection === "AFN") {
       document.getElementById('exchange').innerText = AFN;
     } else if (currencySelection === "ALL") {
@@ -31,6 +31,8 @@ function getExchange() {
       document.getElementById('exchange').innerText = AMD;
     } else if (currencySelection === "ANG") {
       document.getElementById('exchange').innerText = ANG;
+    }  else {
+      document.getElementById('exchange').innerText = "The currency in question doesn't exist";
     }
     }
   
