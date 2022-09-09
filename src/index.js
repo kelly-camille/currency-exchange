89d155f7738c4d5cb81e6a0553b56c6d6eb49694
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-09-09 14:31:07 -0700

    calculate currency exchange

--- src/index.js
+++ src/index.js
@@ -15,18 +15,22 @@ function getExchange() {
   // ui
   function printElements(data) {
     const currencySelection = document.getElementById("currency-selection").value;
-    const AED = 1 * `${[data[0].conversion_rates.AED]}`
+    const AED = document.getElementById("dollar").value * `${[data[0].conversion_rates.AED]}`
+    const AFN = document.getElementById("dollar").value * `${[data[0].conversion_rates.AFN]}`
+    const ALL = document.getElementById("dollar").value * `${[data[0].conversion_rates.ALL]}`
+    const AMD = document.getElementById("dollar").value * `${[data[0].conversion_rates.AMD]}`
+    const ANG = document.getElementById("dollar").value * `${[data[0].conversion_rates.ANG]}`
 
     if (currencySelection === "AED") {
       document.getElementById('exchange').innerText = AED;
     } else if (currencySelection === "AFN") {
-      document.getElementById('exchange').innerText = `${[data[0].conversion_rates.AFN]}`;
+      document.getElementById('exchange').innerText = AFN;
     } else if (currencySelection === "ALL") {
-      document.getElementById('exchange').innerText = `${[data[0].conversion_rates.ALL]}`;
+      document.getElementById('exchange').innerText = ALL;
     } else if (currencySelection === "AMD") {
-      document.getElementById('exchange').innerText = `${[data[0].conversion_rates.AMD]}`;
+      document.getElementById('exchange').innerText = AMD;
     } else if (currencySelection === "ANG") {
-      document.getElementById('exchange').innerText = `${[data[0].conversion_rates.ANG]}`;
+      document.getElementById('exchange').innerText = ANG;
     }
     }
   
@@ -39,3 +43,4 @@ function getExchange() {
   window.addEventListener("load", function() {
     document.querySelector("form").addEventListener("submit", formHandler);
   }) 
+
